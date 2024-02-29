56250de5b34072bdbb0d75e72499351217579789
Gabriel-l-c <gabriellyra.c@gmail.com>
2024-02-29 18:53:17 -0300

    primeira_atualiazação

---
+++ javascript.js
@@ -0,0 +1,14 @@
+document.querySelector('#menu-btn').addEventListener('click', () => {
+    document.querySelector('.list').classList.add('active');
+  });
+
+/* let list = document.querySelector('.list');
+document.querySelector('#menu-btn').onclick = () => {
+  list.classList.toggle('active-list');
+} */
+
+/* window.onscroll = () => {
+  if (window.innerWidth < 700) {
+    navbar.classList.remove('active');
+  }
+} */
